750b1a74a9cac435ea0cbda34d54687f02748d51
jp <janphilipp.ressler@stud.uni-goettingen.de>
2024-12-01 08:40:48 +0200

    added template

---
+++ solution_00.py
@@ -0,0 +1,49 @@
+# =============== imports ===============
+import re, copy
+import itertools as it
+import functools as ft
+import collections as col
+import queue
+from math import *
+from typing import *
+
+from aocd import get_puzzle, submit
+
+# =============== defaults ===============
+c = lambda s: complex(s.replace(',', '+') + 'j')
+
+
+# =============== preparation ===============
+
+puzzle = get_puzzle(year=2024, day=-1)
+data = puzzle.input_data.splitlines()
+
+
+# =============== part 1 ===============
+
+def f1():
+    pass
+
+
+# =============== part 2 ===============
+
+def f2():
+    pass
+
+
+# =============== main ===============
+
+from aocd import submit
+
+def main():
+    a = f1()
+    print(a)
+    # submit(a)
+
+    b = f2()
+    # print(b)
+    # submit(b)
+
+if __name__ == "__main__":
+    main()
+
