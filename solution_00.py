255ab68daa3d69e7d24804a44c6450644b44e159
jp <janphilipp.ressler@stud.uni-goettingen.de>
2024-12-01 09:11:09 +0200

    added support to submit from solution file (even for different sessions)

--- solution_00.py
+++ solution_00.py
@@ -7,40 +7,40 @@ import queue
 from math import *
 from typing import *
 
+# =============== aocd ===============
 from aocd import get_puzzle, submit
+from aocd.models import _load_users
 
-# =============== defaults ===============
+year, day = 2024, 0
+user = "github"
+# user = "reddit"
+aocd_session = _load_users()[user]
+
+puzzle = get_puzzle(year=year, day=day, session=aocd_session)
+
+# =============== snippets ===============
 c = lambda s: complex(s.replace(',', '+') + 'j')
 
-
 # =============== preparation ===============
-
-puzzle = get_puzzle(year=2024, day=-1)
 data = puzzle.input_data.splitlines()
 
-
 # =============== part 1 ===============
-
 def f1():
     pass
 
-
 # =============== part 2 ===============
-
 def f2():
     pass
 
-
 # =============== main ===============
-
 def main():
     a = f1()
     print(a)
-    # submit(a)
+    # submit(a, part="a", session=aocd_session)
 
     b = f2()
-    # print(b)
-    # submit(b)
+    print(b)
+    # submit(b, part="b", session=aocd_session)
 
 if __name__ == "__main__":
     main()
--- solution_01.py
+++ solution_01.py
@@ -7,52 +7,55 @@ import queue
 from math import *
 from typing import *
 
+# =============== aocd ===============
 from aocd import get_puzzle, submit
+from aocd.models import _load_users
 
-# =============== defaults ===============
+year, day = 2024, 1
+user = "github"
+# user = "reddit"
+aocd_session = _load_users()[user]
+
+puzzle = get_puzzle(year=year, day=day, session=aocd_session)
+
+# =============== snippets ===============
 c = lambda s: complex(s.replace(',', '+') + 'j')
 
-
 # =============== preparation ===============
-
-puzzle = get_puzzle(year=2024, day=1)
 data = puzzle.input_data.splitlines()
 
-ll = [map(int, s.split()) for s in data]
-a, b = zip(*ll)
+ll = [map(int, line.split()) for line in data]
+la, lb = zip(*ll)
 
 # =============== part 1 ===============
-
 def f1():
     res = 0
-    for c, d in zip(sorted(a), sorted(b)):
+    for c, d in zip(sorted(la), sorted(lb)):
         res += abs(c - d)
     return res
 
 
 # =============== part 2 ===============
-
 def f2():
-    ca = col.Counter(a)
-    cb = col.Counter(b)
+    ca = col.Counter(la)
+    cb = col.Counter(lb)
 
     res = 0
-    for v, q in ca.items():
-        res += q * v * cb.get(v, 0)
+    for v in ca:
+        res += v * ca.get(v) * cb.get(v, 0)
 
     return res
 
 
 # =============== main ===============
-
 def main():
     a = f1()
     print(a)
-    # submit(a)
+    submit(a, part="a", session=aocd_session)
 
     b = f2()
     print(b)
-    # submit(b)
+    submit(b, part="b", session=aocd_session)
 
 if __name__ == "__main__":
     main()
