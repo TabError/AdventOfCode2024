c6971cf7857cc8498c12d71e66df784e61e2c5bc
jp <janphilipp.ressler@stud.uni-goettingen.de>
2024-12-02 08:01:15 +0200

    updated template

--- solution_00.py
+++ solution_00.py
@@ -10,14 +10,17 @@ from typing import *
 # =============== aocd ===============
 from AOC import AOC
 
-api = AOC(1, 2024, "github")
-# api = AOC(1, 2024, "reddit")
+api = AOC(0, 2024, "github")
 
 # =============== snippets ===============
 c = lambda s: complex(s.replace(',', '+') + 'j')
 
 # =============== preparation ===============
 data = api.input().splitlines()
+# data = open(0).read().splitlines()
+
+# ll = [list(map(int, line.split())) for line in data]
+
 
 # =============== part 1 ===============
 def f1():
@@ -36,6 +39,11 @@ def main():
     b = f2()
     print(b)
     # api.submit_b(b)
+
+
+    import datetime as dt
+    print(dt.datetime.now().strftime("%T:%f")[:-3])
+
 
 if __name__ == "__main__":
     main()
