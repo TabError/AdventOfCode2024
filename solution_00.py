a9bdae10e29abab98f35ce1207521327d279cda5
jp <janphilipp.ressler@stud.uni-goettingen.de>
2024-12-10 16:40:56 +0200

    updated template

--- solution_00.py
+++ solution_00.py
@@ -6,6 +6,7 @@ import collections as col
 import queue
 from math import *
 from typing import *
+import bisect
 
 import datetime as dt
 
@@ -18,15 +19,17 @@ handler: IOHandler = AOC(0, 2024, "github", live=bool(live))
 
 # =============== snippets ===============
 c = lambda s: complex(s.replace(',', '+') + 'j')
-dirs = ((0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1))
-check_coords = lambda i, j: 0 <= i < m and 0 <= j < n
+dirs = (1, -1, 1j, -1j, 1 + 1j, -1 + 1j, 1 - 1j, -1 - 1j)
 
 # =============== preparation ===============
 data = handler.input().splitlines()
 # ll = [list(map(int, line.split())) for line in data]
+# m, n = len(data), len(data[0])
 
 
 # =============== part a ===============
+check_coords = lambda c: 0 <= c.imag < m and 0 <= c.real < n
+
 def part_a():
     res = 0
 
@@ -42,7 +45,6 @@ def part_b():
 def main():
     handler.submit_a(part_a())
     # handler.submit_b(part_b())
-
 
     print(dt.datetime.now().strftime("%T:%f")[:-3])
     print()
